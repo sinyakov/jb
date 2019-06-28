9f0d2e0799db627518379460f0001b23f0407b08
Maksim Sinyakov <masinyakov@avito.ru>
2019-06-29 01:01:17 +0300

    add list css styles

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,33 +1,54 @@
 import React, { useContext } from "react";
+import cn from "classnames";
+
 import { SectionsContext } from "../../contexts/SectionsContext";
+import "./index.css";
 
 export function List({ pages }) {
   const { anchorsDict, pagesDict, currentPage, onCurrentPageChange } = useContext(SectionsContext);
 
   return (
-    <div>
+    <div className="tree">
       {pages.map(pageId => {
         const { title, url, pages, level, anchors, unfolded } = pagesDict[pageId];
+        const isCurrentPage = pageId === currentPage;
+
+        const titleClassName = cn({
+          tree__title: true,
+          tree__title_foldable: pages,
+          tree__title_unfolded: unfolded,
+          tree__title_active: isCurrentPage,
+          [`tree__title_level${level}`]: true,
+        });
+
+        const sectionClassName = cn({
+          tree__section: true,
+          tree__section_current: isCurrentPage,
+        });
 
         return (
-          <div key={pageId} style={{ marginLeft: level * 16 }}>
-            <a onClick={() => onCurrentPageChange(pageId)}>{title}</a>
-            {pages && unfolded && <List pages={pages} />}
-            {pageId === currentPage &&
-              anchors &&
-              anchors.map(anchorId => {
-                const { anchor, title, url } = anchorsDict[anchorId];
+          <div key={pageId}>
+            <div className={sectionClassName}>
+              <a onClick={() => onCurrentPageChange(pageId)} className={titleClassName}>
+                {title}
+              </a>
+              {isCurrentPage &&
+                anchors &&
+                anchors.map(anchorId => {
+                  const { title, url } = anchorsDict[anchorId];
+                  const anchorClassName = cn({
+                    tree__title: true,
+                    [`tree__title_level${level + 1}`]: true,
+                  });
 
-                return (
-                  <a
-                    key={anchorId}
-                    href={url + anchor}
-                    style={{ marginLeft: 16, display: "block" }}
-                  >
-                    {title}
-                  </a>
-                );
-              })}
+                  return (
+                    <a key={anchorId} className={anchorClassName}>
+                      {title}
+                    </a>
+                  );
+                })}
+            </div>
+            {pages && unfolded && <List pages={pages} />}
           </div>
         );
       })}
